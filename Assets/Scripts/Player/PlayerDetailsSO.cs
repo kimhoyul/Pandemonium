b6af3621dc210e7ec92c0b1eb1a39e2991bd4db0
kimhoyul <96504568+kimhoyul@users.noreply.github.com>
2023-09-10 23:20:03 +0900

    67

--- Assets/Scripts/Player/PlayerDetailsSO.cs
+++ Assets/Scripts/Player/PlayerDetailsSO.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "PlayerDetails_", menuName = "Scriptable Objects/Player/PlayerDetailsSO")]
@@ -20,6 +21,13 @@ public class PlayerDetailsSO : ScriptableObject
     public int playerHealthAmount;
 
     [Space(10)]
+    [Header("WEAPON")]
+    [Tooltip("플레이어의 초기 시작 무기")]
+    public WeaponDetailsSO startingWeapon;
+    [Tooltip("플레이어의 초기 시작 무기로 채워주세요.")]
+    public List<WeaponDetailsSO> startingWeaponList;
+
+    [Space(10)]
     [Header("OTHER")]
     [Tooltip("미니맵에서 사용할 플레이어 아이콘 스프라이트를 넣어주세요.")]
     public Sprite playerMinimapIcon;
@@ -34,9 +42,11 @@ public class PlayerDetailsSO : ScriptableObject
         HelperUtilities.ValidateCheckEmptyString(this, nameof(playerCharacterName), playerCharacterName);
         HelperUtilities.ValidateCheckNullValue(this, nameof(playerPrefab), playerPrefab);
         HelperUtilities.ValidateCheckPositiveValue(this, nameof(playerHealthAmount), playerHealthAmount, false);
+        HelperUtilities.ValidateCheckNullValue(this, nameof(startingWeapon), startingWeapon);
         HelperUtilities.ValidateCheckNullValue(this, nameof(playerMinimapIcon), playerMinimapIcon);
         HelperUtilities.ValidateCheckNullValue(this, nameof(playerHandSprite), playerHandSprite);
         HelperUtilities.ValidateCheckNullValue(this, nameof(runtimeAnimatorController), runtimeAnimatorController);
+        HelperUtilities.ValidateCheckEnumerableValues(this, nameof(startingWeaponList), startingWeaponList);
     }
 #endif
     #endregion
